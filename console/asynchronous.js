491c942764a30460a5a684fd4054a9ecf4b88c97
NicolaiCushnir <cushnirnicolai@gmail.com>
2025-03-12 03:05:53 +0200

    deleted all temporaries files

---
+++ console/asynchronous.js
@@ -0,0 +1,8 @@
+// Project learn-javascript :
+
+
+
+
+
+
+
--- console/check.js
+++ console/check.js
@@ -1,11 +1,1 @@
 
-let arr = [0, 2, 3, 1, -2];
-let sum = 0;
-
-for(var i = 0; i < arr.length; i++) {
-	if(arr[i] > 0) {
-		sum += arr[i];
-	}
-
-	console.log( sum );
-}
